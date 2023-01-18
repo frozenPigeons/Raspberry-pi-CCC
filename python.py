c8bd759cf56b1978636dea429c00015cde446935
Mujtaba <118379320+frozenPigeons@users.noreply.github.com>
2023-01-18 11:07:20 -0500

    Add files via upload

--- python.py
+++ python.py
@@ -5,6 +5,40 @@ Switches = [16, 25, 19, 9, 12, 13, 6]
 Buttons = [17, 27, 22, 26]
 
 GPIO.setmode(GPIO.BCM)
+def A():
+    Aarr = [False, False, False, False, False, False, True]
+
+    for i in range(len(Switches)):
+        GPIO.output(Switches[i], Aarr[i])
+
+
+
+def B():
+    Barr = [False, False, False, False, False, False, False]
+
+    for i in range(len(Switches)):
+        GPIO.output(Switches[i], Barr[i])
+
+
+def C():
+
+    Carr = [False, False, False, True, True, True, False]
+
+    for i in range(len(Switches)):
+        GPIO.output(Switches[i], Carr[i])
+
+def D():
+
+    Darr = [False, False, False, True, False, False, False]
+
+    for i in range(len(Switches)):
+        GPIO.output(Switches[i], Darr[i])
+
+def E():
+    Earr = [False, False, False, False, True, True, False]
+
+    for i in range(len(Switches)):
+        GPIO.output(Switches[i], Earr[i])
 
 for i in Buttons:
     GPIO.setup(i, GPIO.IN, pull_up_down=GPIO.PUD_UP)
@@ -35,35 +69,64 @@ while GPIO.input(17) != False:
 if(GPIO.input(17) == False):
     print(songs)
 
-def Letters(digit, letter):
-    GPIO.output(Digits[digit], True)
-    if letter == "A":
-        Aarr = [False, False, False, False, False, False, True]
-        for i in range(len(Switches)):
-            GPIO.output(Switches[i], Aarr[i])
-    elif letter == "B":
-        Barr = [False, False, False, False, False, False, False]
-        for i in range(len(Switches)):
-            GPIO.output(Switches[i], Barr[i])
-    elif letter == "C":
-        Carr = [False, False, False, True, True, True, False]
-        for i in range(len(Switches)):
-            GPIO.output(Switches[i], Carr[i])
-    elif letter == "D":
-        Darr = [False, False, False, True, False, False, False]  
-        for i in range(len(Switches)):
-            GPIO.output(Switches[i], Darr[i])
-    else: 
-        Earr = [False, False, False, False, True, True, False]
-        for i in range(len(Switches)):
-            GPIO.output(Switches[i], Earr[i])
-
 while True:
     time.sleep(0.001)
-    Letters(0, songs[0])
+    GPIO.output(20, True)
+    GPIO.output(24, False)
+    GPIO.output(23, False)
+    GPIO.output(10, False)
+    if songs[0] == 'A':
+        A()
+    if songs[0] == 'B':
+        B()
+    if songs[0] == 'C':
+        C()
+    if songs[0] == 'D':
+        D()
+    if songs[0] == 'E':
+        E()
     time.sleep(0.001)
-    Letters(1, songs[1])
+    GPIO.output(20, False)
+    GPIO.output(24, True)
+    GPIO.output(23, False)
+    GPIO.output(10, False)
+    if songs[1] == 'A':
+        A()
+    if songs[1] == 'B':
+        B()
+    if songs[1] == 'C':
+        C()
+    if songs[1] == 'D':
+        D()
+    if songs[1] == 'E':
+        E()  
     time.sleep(0.001)
-    Letters(2, songs[2])
+    GPIO.output(20, False)
+    GPIO.output(24, False)
+    GPIO.output(23, True)
+    GPIO.output(10, False)
+    if songs[2] == 'A':
+        A()
+    if songs[2] == 'B':
+        B()
+    if songs[2] == 'C':
+        C()
+    if songs[2] == 'D':
+        D()
+    if songs[2] == 'E':
+        E()  
     time.sleep(0.001)
-    Letters(3, songs[3])
+    GPIO.output(20, False)
+    GPIO.output(24, False)
+    GPIO.output(23, False)
+    GPIO.output(10, True)
+    if songs[3] == 'A':
+        A()
+    if songs[3] == 'B':
+        B()
+    if songs[3] == 'C':
+        C()
+    if songs[3] == 'D':
+        D()
+    if songs[3] == 'E':
+        E()  
